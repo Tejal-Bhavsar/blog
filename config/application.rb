e1d74449d2f3b828a903c8092edbee2b6295e65f
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-04-27 23:54:17 +0530

    error removed and config added

--- config/application.rb
+++ config/application.rb
@@ -1,24 +1,23 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
-require "devise" 
-require File.expand_path('../boot', __FILE__)
+require "devise"
+require File.expand_path("../boot", __FILE__)
 
-require 'csv'
-
- 
+require "csv"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 Bundler.require
-  
+
 module Blog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
