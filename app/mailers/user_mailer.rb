ed41a4e8f34ad5b99c8943a63b500a3434ed0a13
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-04-27 23:10:07 +0530

    added environment varialbles

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
-    @url = "http://localhost:3000/users/sign_in"
+    @url = ENV["app_url"]/users/sign_in"
     mail(to: @user.email, subject: "Welcome to My Awesome blog Site")
   end
 end
